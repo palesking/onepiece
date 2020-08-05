3acdc5080d3200cfe6a18d871e7ebce1bc399108
palesking <525440245@qq.com>
2020-08-05 20:36:35 +0800

    1

---
+++ uniapp/hello-uniapp/pages/contact/contact.vue
@@ -0,0 +1,9 @@
+<template>
+	<view>这是我们</view>
+</template>
+
+<script>
+</script>
+
+<style>
+</style>
---
+++ uniapp/hello-uniapp/pages/details/details.vue
@@ -0,0 +1,16 @@
+<template>
+	<view>
+		<view>
+			<text>详情页面</text>
+		</view>
+		<view>
+			<text selectable>唱歌跳舞打篮球</text>
+		</view>
+	</view>
+</template>
+
+<script>
+</script>
+
+<style>
+</style>
