3bad46e2dabe2a35e5f30452acbe1cb499310f7e
palesking <525440245@qq.com>
2020-08-06 16:33:52 +0800

    1

--- uniapp/hello-uniapp/pages/details/details.vue
+++ uniapp/hello-uniapp/pages/details/details.vue
@@ -4,8 +4,27 @@
 			<text>详情页面</text>
 		</view>
 		<view>
-			<text selectable>唱歌跳舞打篮球</text>
+			<text selectable space="emsp">唱歌 跳舞打篮球</text>
 		</view>
+		<view>
+			<text selectable space="ensp">唱歌 跳舞打篮球</text>
+		</view>
+		<view> 
+			<text selectable space="nbsp" style="font-size: 20px;color: #4CD964;">唱歌    跳舞打篮球</text>
+		</view>
+		<view>
+			<text decode>唱歌&nbsp;&amp;跳舞打篮球</text>
+		</view>
+		<view class="box" hover-class="boxactive" :hover-start-time="2000" :hover-stay-time="2000">大盒子
+			<view class="boxone" hover-class="box1active" hover-stop-propagation>
+				小盒子
+			</view>
+		</view>
+		<button type="default">按钮</button>
+		<button type="default" size="mini">按钮</button>
+		<button type="default" plain>按钮</button>
+		<button type="default" disabled>按钮</button>
+		<button type="default" loading>按钮</button>
 	</view>
 </template>
 
@@ -13,4 +32,20 @@
 </script>
 
 <style>
+	.box{
+		width: 100px;
+		height: 100px;
+		background-color: #3F536E;
+	}
+	.boxactive{
+		background-color: #4CD964;
+	}
+	.boxone{
+		width: 50px;
+		height: 50px;
+		background-color: pink;
+	}
+	.box1active{
+		background-color: green; 
+	}
 </style>
