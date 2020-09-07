ae84639ef6114539e947a0c9245999a290aa671e
palesking <525440245@qq.com>
2020-09-08 00:10:58 +0800

    1

---
+++ myblog/src/components/header/index.vue
@@ -0,0 +1,41 @@
+<template>
+    <div class="header-content container">
+        <a href="/" class="logo testone">
+            <!-- <span>天</span>
+            <span>堂</span>
+            <span>瀑</span>
+            <span>布</span> -->
+            <img src="../../assets/logo2.png" alt="">
+        </a>
+        <div class="content-title">
+            <ul class="layui-nav layui-this" lay-filter="">
+                <li class="layui-nav-item">
+                    <router-link to='/'>首页</router-link>
+                </li>
+                <li class="layui-nav-item">
+                    <router-link to='/version'>说说</router-link>
+                </li>
+                <li class="layui-nav-item">
+                    <router-link to='/movie'>电影</router-link>
+                </li>
+                <li class="layui-nav-item">
+                    <router-link to='/community'>文章</router-link>
+                </li>
+                <li class="layui-nav-item">
+                    <router-link to='/music'>音乐</router-link>
+                </li>
+                <li class="layui-nav-item">
+                    <router-link to='/about'>关于</router-link>
+                </li>
+            </ul>
+        </div>
+    </div>
+</template>
+<script>
+export default {
+    
+}
+</script>
+<style lang="less" scoped>
+    
+</style>
